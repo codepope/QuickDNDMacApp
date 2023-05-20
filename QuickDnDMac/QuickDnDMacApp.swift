384dff50ca6637945b1c45c014bb891b33dc3f1a
Dj Walker-Morgan <codepope@gmail.com>
2023-05-20 19:12:17 +0100

    Initial Commit

---
+++ QuickDnDMac/QuickDnDMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickDnDMacApp.swift
+//  QuickDnDMac
+//
+//  Created by Dj Walker-Morgan on 20/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct QuickDnDMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
